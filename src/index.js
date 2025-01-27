380500736aaa8ab950ace99f2aa54b14161a32a4
Ferly Jeremi Purnawan Apiang <ferly.apiang9@gmail.com>
2025-01-27 16:41:59 +0700

    Update Fetch bundle

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 3000;
 // Menyajikan file statis dari folder public  
 app.use(express.static(path.join(__dirname, '../public')));  
   
+// Menyajikan file index.html sebagai halaman utama  
+app.get('/', (req, res) => {  
+    res.sendFile(path.join(__dirname, '../public/index.html'));  
+});  
+  
+// Menangani rute yang tidak ditemukan  
+app.use((req, res) => {  
+    res.status(404).send('404: Halaman tidak ditemukan');  
+});  
+  
+// Menjalankan server  
 app.listen(port, () => {  
     console.log(`Server is running at http://localhost:${port}`);  
 });  
